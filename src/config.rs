dbe0954fdf6e4c9b652704d6bc1a365401276806
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-23 08:23:55 -0700

    config

---
+++ src/config.rs
@@ -0,0 +1,13 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Config {
+
+}
+
+impl Config {
+    pub fn from_file(path: &str) -> anyhow::Result<Self> {
+        let config_str = std::fs::read_to_string(path)?;
+        Ok(serde_json::from_str(&config_str)?)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use tracing::error;
+use crate::config::Config;
 
 mod server;
+mod bazel;
+mod config;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -9,10 +11,17 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt().with_writer(non_blocking).init();
     
     run_basil().await.inspect_err(|err| {
-        error!("{}", err.to_string())        
+        tracing::error!("{}", err.to_string())        
     })
 }
 
 async fn run_basil() -> anyhow::Result<()> {
+    let config_path = std::env::args().next();
+
+    let config = match config_path {
+        Some(path) => Config::from_file(&path),
+        None => Err(anyhow::anyhow!("Missing basil config file arg"))
+    }?;
+
     Ok(())
 }

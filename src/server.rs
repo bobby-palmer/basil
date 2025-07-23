b8be1bf74c60a7638809cbf7b66215203f1a6a49
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-22 18:43:00 -0700

    not sure

--- src/server.rs
+++
@@ -1,9 +0,0 @@
-use crate::{io, config};
-
-pub async fn run<R, W>(mut reader: R, mut writer: W, config: config::Config) -> anyhow::Result<()> 
-where
-    R: io::MessageReader,
-    W: io::MessageWriter
-{
-    Ok(())
-}

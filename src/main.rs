d2ab15b2b031897c4d57b3df39e76b9a530a587e
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-23 08:51:51 -0700

    config

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,11 @@ mod server;
 async fn main() -> anyhow::Result<()> {
     let file_appender = tracing_appender::rolling::never(".basil", "logs");
     let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    tracing_subscriber::fmt().with_writer(non_blocking).init();
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .with_ansi(false)
+        .with_env_filter("debug")
+        .init();
 
     run_basil()
         .await
@@ -17,10 +21,11 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn run_basil() -> anyhow::Result<()> {
-    let config_path = std::env::args().next();
+    let config_path = std::env::args().skip(1).next();
 
-    let config = match config_path {
-        Some(path) => Config::from_file(&path),
+    let _config = match config_path {
+        Some(path) => Config::from_file(&path)
+            .map_err(|err| err.context(format!("Cannot read config from {}", path))),
         None => Err(anyhow::anyhow!("Missing basil config file arg")),
     }?;
 

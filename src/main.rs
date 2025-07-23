878924f86d4996bc05e6661df3d1b3332ba619ba
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-23 07:48:05 -0700

    logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use tracing::error;
+
+mod server;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let file_appender = tracing_appender::rolling::never(".basil", "logs");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt().with_writer(non_blocking).init();
+    
+    run_basil().await.inspect_err(|err| {
+        error!("{}", err.to_string())        
+    })
+}
+
+async fn run_basil() -> anyhow::Result<()> {
     Ok(())
 }

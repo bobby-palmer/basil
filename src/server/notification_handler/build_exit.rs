15c80acc1334b6373993c409d33a9749ebe8cbcc
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-24 21:41:34 -0700

    exit

--- src/server/notification_handler/build_exit.rs
+++ src/server/notification_handler/build_exit.rs
@@ -2,6 +2,7 @@ pub struct BuildExitHandler;
 
 impl super::NoParamsHandler for BuildExitHandler {
     async fn handle(ctx: &crate::server::context::Context) {
+        // std::process::exit(0);
         todo!()
     }
 }
